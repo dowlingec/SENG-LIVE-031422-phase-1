1015d7971bdf772af7eddd1427dae6d5e5a1905a
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-16 15:05:14 -0400

    pokemon pt1

--- 03-js-arrays/index.js
+++ 03-js-arrays/index.js
@@ -2,20 +2,34 @@
 
 // What are arrays and how do they allow us to structure our data?
 
-// Define a variable 'pokemon' set to an empty array.
-const pokemon = ['']
+// Define a variable 'pokemon' set to an empty array (no quotation marks etc inside square brackets).
+//const pokemon = []
 // Rewrite the 'pokemon' array with the following elements: 'pikachu', 'bulbasour', 'jigglypuff'
-
-// Elements:
+const pokemon = ['pikachu', 'bulbasaur', 'jigglypuff']
+// Elements: ITEM INSIDE ARRAY as above 'pikachu' is an element
 
 // Return 'bulbasour' from the pokemon array
-
+pokemon[1]
+//elements can be reached using other moethods like pokemon[pokemon.length-1]
 // Modifying elements:
 
+
 // Add a new element 'mew' to the end of pokemon. There are 2 ways this can be accomplished. Which method is best practice and why?
+pokemon.push("mew")
+//.push() ia destructive way to add to the end of an array
+
+//const newPokemonArray =[...pokemon, 'Mew']
+//spread operator makes a shallow copy of array and then modifies, leaving original array untouched to add to beginning or end. Above showsto end, below to beignning
+//const newPokemonArray = ['mew', ...pokemon]
+
+
+
+
+
 
 // Add a new element 'Gloom' to the beginning of pokemon.
-
+pokemon.unshift('Gloom')
+//slice - adds to beginning of array in a new copy with arguments (eg certain elements)
 // Remove the first element of 'pokemon'
 
 // Create a new array, 'favorites', that includes the first 2 elements from pokemon only and the following 2 new elements: 'squirtle', 'turtwig'.
