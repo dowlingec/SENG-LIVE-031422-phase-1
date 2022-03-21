9b323bfcb2960fb3b73e87477c3889620187d120
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-21 15:58:07 -0400

    eventListeners

--- 06-dom-events/index.js
+++ 06-dom-events/index.js
@@ -1,3 +1,12 @@
+//textContent good for text rendered inside of an HTL element
+// innerHTML want to update the actual eleents of another element
+//innerText something used to set text for an element
+// you can write a function to help dreate divs without having to write full code each time, we will learn at a later date
+// append will accept a string
+// append child has to be direct node createElement creates a node
+//DOM is tre of js nodes. createElement creats a node
+//debugger allows code to run up to debugger call, within a function or global scope
+
 const pokemon = [
   {
     id: 1,
@@ -46,9 +55,93 @@ function renderPokemon(char) {
   pokeImg.src = char.img;
   pokeImg.alt = `${char.name} image`;
 
+  const pokeLikes = document.createElement("h3")
+  pokeLikes.textContent = "Likes: "
+
   const pokeName = document.createElement("h3");
   pokeName.textContent = char.name;
 
-  pokeCard.append(pokeImg, pokeName);
+  const likesNum = document.createElement("h5")
+  likesNum.className = "likes-num";
+  likesNum.textContent = char.likes;
+
+  const likesBttn = document.createElement('button')
+  likesBttn.className = 'like-bttn';
+  likesBttn.textContent = "♥";
+
+  const deleteBttn = document.createElement('button')
+  deleteBttn.className = 'delete-bttn';
+  deleteBttn.textContent = "Delete";
+
+  pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likesBttn, deleteBttn);
   pokeContainer.appendChild(pokeCard);
+
 }
+
+//remove elemnts from the DOM
+//.remove()
+//first select element to be removed then call .remove()
+
+// const lectureDiv = document.querySelector('#lecture-goals')
+// lectureDiv.remove()
+// //or
+// const lectureDiv = document.querySelector('#lecture-goals').remove()
+
+//update Elements on the DOM
+//select the element that we are updating and then update 
+// document.getElementById("header").innerHTML = `<img id="header-img" src="https://external-preview.redd.it/tQged7mKJ3cUpNMq5IMeceZvyKP3cTyHqhNmKEQ0Vv8.png?auto=webp&s=fb5fd61cae0bc9cde2bc2a006b1e2aeb0c935ce9" />`
+
+
+function replaceHeader() {
+  const header = document.getElementById('header')
+  const img = document.createElement('img')
+  img.id = "header-img"
+  img.src = "https://external-preview.redd.it/tQged7mKJ3cUpNMq5IMeceZvyKP3cTyHqhNmKEQ0Vv8.png?auto=webp&s=fb5fd61cae0bc9cde2bc2a006b1e2aeb0c935ce9"
+  header.replaceChildren(img)
+}
+replaceHeader()
+
+//static vs dynamic web pages
+//Static - just plain display 
+//Dynamic - changes with user input
+
+// events: wait for some type of action that interacts with the html elements 
+// event listener: listens for type of event and fire off event handler
+//event type: action we are listening for eg click, mouseover, submit, keyDown, keyUp, DOMContent
+// event handler: function responsible for executing taks after and event has occured
+// we can attach event to element by :
+// inline with html element OR .addEventListener('event type', callbackFunction)
+
+//work flow:
+// 1. select element that and event will be created for
+// 2. Attach eventListener to target & decide what even is being listened for & what event handler is going to execute
+// submit events are only available for forms
+// click event only for buttons
+// preventDefault() prevent default behaviour of element & inject our own
+//event object produced when event  occurs, has properties and methods that can be called on it, info most commonly use: target
+
+//grab target
+const pokeForm = document.getElementById('poke-form')
+//attach event listener to target
+pokeForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // form is submitted, need to collect values of form by checking the.value of each input
+  const name = document.querySelector('#name-input').value
+  console.log(name)
+  const img = document.querySelector('#img-input').value
+  console.log(img)
+  //create new charcter object
+  const newChar = {
+    name,
+    img,
+    id: 6, // NEEDS TO CHANGE
+    likes: 0
+  }
+  renderPokemon(newChar)
+  pokeForm.reset()
+})
+
+
+
+//ES6 simple object creation when you have an object key and value assnmnt that are the same, you can combine to a one world value
+// name: name, vs name, it is essentially shorthand, and cleans up the code
